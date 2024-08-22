76b4830c0f261acc9f559acc7b69abf87eb39c9a
Mikhail Miakishev <kotarolinn@gmail.com>
2024-08-23 01:00:22 +0300

    Added possibility to update a producer

--- Models/Producer.cs
+++ Models/Producer.cs
@@ -8,13 +8,14 @@ namespace eTickets.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "The Profile Picture is Required")]
         [Display(Name = "ProfilePictureURL")]
         public string ProfilePictureURL { get; set; }
-        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars")]
+        [Required(ErrorMessage = "The Full Name is Required")]
         [Display(Name = "FullName")]
         public string FullName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "The Biography is Required")]
         [Display(Name = "Biography")]
         public string Bio { get; set; }
 
