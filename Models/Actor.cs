50cddda7e7ef6da81ba91d7d571c1215d3fb43c7
Mikhail Miakishev <kotarolinn@gmail.com>
2024-08-22 15:42:51 +0300

    Added Actor Model

---
+++ Models/Actor.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string ProfilePictureURL { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string Bio { get; set; }
+    }
+}
