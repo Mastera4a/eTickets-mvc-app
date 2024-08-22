43acb80a32dc5d5c0a5b8f1d528beb78a558409b
Mikhail Miakishev <kotarolinn@gmail.com>
2024-08-22 15:45:11 +0300

    Added Cinema Model

---
+++ Models/Cinema.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Logo { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
