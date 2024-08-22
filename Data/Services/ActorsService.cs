c0c8f2887e945b273a1a770062ee12e69270c5a7
Mikhail Miakishev <kotarolinn@gmail.com>
2024-08-22 19:11:56 +0300

    GetAllAsync() & GetByIdAsync() base methods

--- Data/Services/ActorsService.cs
+++ Data/Services/ActorsService.cs
@@ -11,12 +11,6 @@ namespace eTickets.Data.Services
             _context = context;
         }
 
-        public void Add(Actor actor)
-        {
-            _context.Actors.Add(actor);
-            _context.SaveChanges();
-        }
-
         public async Task AddAsync(Actor actor)
         {
             await _context.Actors.AddAsync(actor);
@@ -30,20 +24,6 @@ namespace eTickets.Data.Services
                 .LastOrDefaultAsync(a => a.Id == id);
             _context.Actors.Remove(result);
             await _context.SaveChangesAsync();
-        }
-
-        public async Task<IEnumerable<Actor>> GetAllAsync()
-        {
-            var result = await _context.Actors.ToListAsync();
-            return result;
-        }
-
-        public async Task<Actor> GetByIdAsync(int id)
-        {
-            var result = await _context.Actors
-                .OrderBy(a => a.Id)
-                .LastOrDefaultAsync(a => a.Id == id);
-            return result;
         }
 
 
