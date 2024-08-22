0f76234fa8622a576e84b8990444ec0e368472fb
Mikhail Miakishev <kotarolinn@gmail.com>
2024-08-23 01:30:08 +0300

    ADded possibility to delete cinema from db

--- Controllers/CinemasController.cs
+++ Controllers/CinemasController.cs
@@ -43,5 +43,21 @@ namespace eTickets.Controllers
             if (cinemaDetails == null) return View("NotFound");
             return View(cinemaDetails);
         }
+
+        // GET: Cinemas/Edit/{id}
+        public async Task<IActionResult> Edit(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Logo,Name,Description")] Cinema cinema)
+        {
+            if (!ModelState.IsValid) return View(cinema);
+            await _service.UpdateAsync(id, cinema);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
